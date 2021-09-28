dc31eddcc564561bda9d70f6e5d9c541aaddf56d
potykion <potykion@gmail.com>
2021-09-28 17:00:15 +0300

    7 done

--- src/7_Maximum Subarray.py
+++ src/7_Maximum Subarray.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 
@@ -6,6 +7,18 @@ class Solution:
     https://leetcode.com/problems/maximum-subarray/
 
     >>> s = Solution()
+    >>> nums = [3,2,-3,-1,1,-3,1,-1]
+    >>> s.maxSubArray(nums)
+    5
+    >>> nums = [-1,-2,3,-1,-2,1,1]
+    >>> s.maxSubArray(nums)
+    3
+    >>> nums = [-1,1,-1,2,-2,-2]
+    >>> s.maxSubArray(nums)
+    2
+    >>> nums = [1,1,-2]
+    >>> s.maxSubArray(nums)
+    2
     >>> nums = [1,2,3]
     >>> s.maxSubArray(nums)
     6
@@ -18,21 +31,62 @@ class Solution:
     >>> nums = [5,4,-1,7,8]
     >>> s.maxSubArray(nums)
     23
+    >>> nums = [-1]
+    >>> s.maxSubArray(nums)
+    -1
+
     """
 
     def maxSubArray(self, nums: List[int]) -> int:
-        arrays = (
-            nums[inner:][:outer + 1]
-            for inner in range(len(nums))
-            for outer in range(len(nums[inner:]))
-        )
-        return max(map(sum, arrays))
+        max_sum = sum(nums)
 
+        result = nums
 
+        while True:
+            deque_ = deque(result)
+            buffer = []
+            sum_acc = 0
+            while deque_:
+                el = deque_.popleft()
+                buffer.append(el)
+                sum_acc += el
+                max_sum = max(max_sum, sum_acc)
+                if sum_acc < 0:
+                    sum_acc = 0
+                    buffer = []
+            result = buffer or nums
 
-if __name__ == '__main__':
-    from src.utils import timeit
-    import json
+            deque_ = deque(result)
+            buffer = []
+            sum_acc = 0
+            while deque_:
+                el = deque_.pop()
+                buffer.append(el)
+                sum_acc += el
+                max_sum = max(max_sum, sum_acc)
+                if sum_acc < 0:
+                    sum_acc = 0
+                    buffer = []
+            new_result = buffer[::-1]
+            if not new_result:
+                result = new_result
+                break
 
-    with open("../data/7.json") as numbers_file:
-        timeit(Solution().maxSubArray, json.load(numbers_file))
+            if len(result) == len(new_result):
+                result = new_result
+                break
+            else:
+                result = new_result
+
+        return max(filter(None, [max_sum, sum(result), max(nums)]), default=0)
+
+# if __name__ == '__main__':
+#     from src.utils import timeit
+#     import json
+#
+#     with open("../data/7_1.json") as numbers_file:
+#         # 0:00:12.843720
+#         # 0:00:08.843720
+#         # 0:00:06.338794
+#         # 0:00:00.023174
+#         timeit(Solution().maxSubArray, json.load(numbers_file))
